52474de97b2368756fc12fe576b0b942a54e3179
jbr-atol <jbr@atolcd.com>
2017-12-05 17:30:40 +0100

    ajout du formulaire d'ajout et de modification de posts

--- blog/urls.py
+++ blog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path('', views.post_list, name ='post_list'),
     re_path('^post/(?P<pk>[0-9]+)/$',views.post_detail,name='post_detail'),
+    re_path('^post/new/$',views.post_new, name='post_new'),
+    re_path('^post/(?P<pk>[0-9]+)/edit/$', views.post_edit, name='post_edit')
 ]
 
