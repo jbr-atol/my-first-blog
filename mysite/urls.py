16425f1f49ce0078b4578cb2ea421eab5c64b785
jbr-atol <jbr@atolcd.com>
2017-12-07 15:20:01 +0100

    installation d'un package pour faire des sondages

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include, re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('blog.urls'))
+    path('',include('blog.urls')),
+    path('polls/', include('polls.urls'))
 ]
